7b8f6336eced00f50cc0eb26d33e4a7b3f12249e
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2016-10-09 16:49:41 -0300

    Add supervisor for develop

--- queries.js
+++ queries.js
@@ -25,11 +25,42 @@ function getAllWells(req, res, next) {
 }
 
 function getSingleWell(req, res, next) {
-  return next(0);
+  var wId = parseInt(req.params.id);
+  db.one('select * from well where id = $1', wId)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE well'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 }
 
+
+// curl --data "nombre=Pozo4&x=450004&y=6500004" http://127.0.0.1:3000/api/wells
 function createWell(req, res, next) {
-  return next(0);
+  // Parseo los datos
+  req.body.nombre = req.body.nombre.toString();
+  req.body.x = parseFloat(req.body.x);
+  req.body.y = parseFloat(req.body.y);
+  // Los inserto
+  db.none('insert into well(nombre, x, y)' +
+      'values(${nombre}, ${x}, ${y})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one well'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 }
 
 function updateWell(req, res, next) {
