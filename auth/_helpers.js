1bea3c6872659e46da779addcc3b899f1dd543bf
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2016-10-16 21:00:49 -0300

    Se agregan eventos a bcrypt passwordHashed y passwordCompared

--- auth/_helpers.js
+++ auth/_helpers.js
@@ -1,25 +1,97 @@
-var bcrypt = require('bcrypt');
-const saltRounds = 10;
+// Para encriptar - bcryptjs esta en todos lados, no tiene dependencias y es
+// compatible con bcrypt
+var bcrypt = require('bcryptjs');
+// Para emitir eventos que desea agregar
+const EventEmitter = require('events');
+class PasswordEmitter extends EventEmitter {}
+const passwordEmitter = new PasswordEmitter();
 
-function createHashPassword(userPlainPassword, cb) {
+passwordEmitter.on('passwordHashed', function (uid, err, hash) {
+  console.log('A passwordHashed event occurred!');
+  console.log('uid: ', uid);
+  console.log('hash: ', hash);
+});
+
+passwordEmitter.on('passwordCompared', function (uid, err, res) {
+  console.log('A passwordCompared event occurred!');
+  console.log('uid: ', uid);
+  console.log('result: ', res);
+});
+
+//var Promise = require('promise');
+
+// const saltRounds = 10;
+
+/*
+function generateSaltP(saltRounds) {
+  saltRounds = saltRounds || 10;
+  return new Promise(function (resolve, reject) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(salt);
+      }
+    })
+  })
+}
+
+function generateSalt(saltRounds) {
+  generateSaltP(saltRounds).then(function (data) {
+      return data;
+    }).catch(function (err) {
+      return err;
+    });
+}
+
+
+function createHashPasswordP(userPlainPassword, salt) {
+  salt = salt || 10;
+  return new Promise(function (resolve, reject) {
+    bcrypt.hash(userPlainPassword, salt, function (err, hash) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(hash);
+    })
+  })
+}
+*/
+
+function createHashPassword(uid, userPlainPassword, saltRounds) {
+  saltRounds = saltRounds || 10;
   // First, generate salt
   bcrypt.genSalt(saltRounds, function(err, salt) {
     // hash is the encrypted password
     bcrypt.hash(userPlainPassword, salt, function(err, hash) {
-        console.log('hash: ', hash);
-        return cb(err, hash);
+      /*
+      console.log('userPlainPassword: ', userPlainPassword);
+      console.log('salt: ', salt);
+      console.log('err: ', err);
+      console.log('hash: ' + hash);
+      */
+      passwordEmitter.emit('passwordHashed', uid, err, hash);
+      //retval = hash;
     });
   });
 }
 
-function comparePassword(userPlainPassword, userHashPassword, cb) {
+function comparePassword(uid, userPlainPassword, userHashPassword) {
   bcrypt.compare(userPlainPassword, userHashPassword, function(err, res) {
-    console.log('res: ',res);
-    return cb(err,res);
+    /*
+    console.log('err: ', err);
+    console.log('res: ', res);
+    */
+    passwordEmitter.emit('passwordCompared', uid, err, res);
   });
 }
 
 module.exports = {
   createHashPassword,
   comparePassword
+  /*
+  generateSaltP,
+  generateSalt,
+  createHashPasswordP
+  */
 }
--- test/test-password.js
+++ test/test-password.js
@@ -1,22 +1,46 @@
-var expect    = require("chai").expect;
+var chai = require('chai');
+/*
+// Expect from chai promises
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+*/
+
+var expect = chai.expect;
+// To be tested
 var he = require("../auth/_helpers");
 
 describe("Bcrypt passwords test", function() {
 
+  /*
+  it("Generate salt with promise", function() {
+    // DEntro de It van expect
+    var saltRounds  = 10;
+
+    var salt = he.generateSalt(saltRounds);
+    console.log('salt: ', salt)
+
+    expect(salt);
+    //expect(hP).to.eventually.be.an('string');
+  });
+  */
+
   it("Create hash password", function() {
     // DEntro de It van expect
+    var uid = 14356;
     var plainPassword  = '11fernando22';
-    var hashPassword = '$2a$10$9Xk4f9anHWzzEtZsz9hMJOjztaldiUcu14CXcHP1MKzfbUx.mlPfC$2a$10$9Xk4f9anHWzzEtZsz9hMJOjztaldiUcu14CXcHP1MKzfbUx.mlPfC';
-    var hP = he.createHashPassword(plainPassword);
-
-    expect(hP).to.equal(hashPassword);
+    var saltRounds  = 10;
+    //var retval;
+    var hP = he.createHashPassword(uid, plainPassword, saltRounds);
+    //expect(hP).to.eventually.be.an('string');
+    expect(hP).to.equal('$2a$10$nMhqaHiR6pKjP0CoU/bruup43wAnOTw7Zkn.uQCPHlRTJ7eBgtQWS');
   });
 
   it("Compare passwords", function() {
     // DEntro de It van expect
+    var uid = 14356;
     var plainPassword  = '11fernando22';
-    var hashPassword = '$2a$10$9Xk4f9anHWzzEtZsz9hMJOjztaldiUcu14CXcHP1MKzfbUx.mlPfC$2a$10$9Xk4f9anHWzzEtZsz9hMJOjztaldiUcu14CXcHP1MKzfbUx.mlPfC';
-    var hP = he.comparePassword(plainPassword,hashPassword);
+    var hashPassword = '$2a$10$nMhqaHiR6pKjP0CoU/bruup43wAnOTw7Zkn.uQCPHlRTJ7eBgtQWS';
+    var hP = he.comparePassword(uid, plainPassword,hashPassword);
 
     expect(hP).to.equal(true);
   });
